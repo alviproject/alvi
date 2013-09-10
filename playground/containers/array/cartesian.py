c681315a2e5aef09370a5beab0f4ea4fe23ae0ae
Piotr Lewalski <piotr@lewalski.pl>
2013-09-10 23:31:10 +0200

    working on booble sort

--- playground/containers/array/cartesian.py
+++ playground/containers/array/cartesian.py
@@ -5,15 +5,15 @@ import playground.spaces.cartesian
 class _Node(base.Node):
     def __init__(self, container, value):
         super().__init__(container)
-        self._point = container._space.create_point(value, container._next_node_id())
+        self._point = container._space.create_point(container._next_node_id(), value)
 
     @property
     def value(self):
-        return self._point.x
+        return self._point.y
 
     @value.setter
     def value(self, value):
-        self._point.x = value
+        self._point.y = value
 
 
 class Cartesian(base.Array):
--- playground/scenes/booble.py
+++ playground/scenes/booble.py
@@ -1,10 +1,7 @@
-from playground.spaces import Sorting
 import random
 
 
 class Booble(object):
-    Space = Sorting
-
     def __init__(self, n):
         self.n = n
 
@@ -13,24 +10,32 @@ class Booble(object):
         array[index_a] = array[index_b]
         array[index_b] = t
 
-    def run(self, array):
+    def init(self, array):
         array.stats.comparisons = 0
         array.stats.assignments = 0
         array.init(self.n)
+        array.sync()
 
+    def generate_points(self, array):
         for i in range(self.n):
             array[i] = random.randint(1, self.n)
         array.sync()
 
+    def sort(self, array):
         changed = True
         while changed:
             changed = False
             for j in range(1, array.size()):
                 item_a = array[j]
                 item_b = array[j - 1]
-                if item_a > item_b:
+                if item_a < item_b:
                     self.swap(array, j, j - 1)
                     changed = True
                     array.stats.assignments += 2
                 array.stats.comparisons += 1
             array.sync()
+
+    def run(self, array):
+        self.init(array)
+        self.generate_points(array)
+        self.sort(array)
