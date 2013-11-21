1064b15f1c7210a92cf2ebc252e4cbd062004fdd
Piotr Lewalski <piotr@lewalski.pl>
2013-11-21 20:51:57 +0100

    working on CI

--- setup.py
+++ setup.py
@@ -13,8 +13,13 @@ setup(
     description='Algorithm Visualization framework',
     long_description='Algorithm Visualization framework',
     install_requires=[
+        'django==1.6',
+        'tornado==3.1.1',
+        'sockjs-tornado==1.0.0',
     ],
     tests_require=[
+        'selenium==2.37.2',
+        'coverage==3.7',
     ],
     test_suite="alvi.tests",
     include_package_data=True,
