deb51a7934e0fe09c4ed891af45567d74f09a18d
Piotr Lewalski <piotr@lewalski.pl>
2013-12-21 20:40:48 +0100

    moving change root functionality from Tree to RBTree

--- alvi/tests/resources/client/local_python_client/scenes/tree/change_root.py
+++
@@ -1,8 +0,0 @@
-from alvi.tests.resources.client.local_python_client.scenes.tree.create_node import TreeCreateNode
-
-
-class TreeChangeRoot(TreeCreateNode):
-    def run(self, tree):
-        super().run(tree)
-        tree.root = self.nodes[1]
-        tree.sync()
--- alvi/tests/test_client/test_tree.py
+++ alvi/tests/test_client/test_tree.py
@@ -43,25 +43,6 @@ class TestTree(TestContainer):
 
         self.assertEqual(expected, node_data, "change_parent does not work properly")
 
-    def test_change_root(self):
-        page = pages.Tree(self._browser.driver, "TreeChangeRoot")
-        page.goto()
-
-        self.assertEqual(7, len(page.svg.nodes), "create_node does not work properly")
-
-        node_data = sorted(page.svg.node_data, key=lambda d: d['id'])
-        expected = [
-            {'id': 0, 'parent': 1, 'name': 0},
-            {'id': 1, 'parent': 0, 'name': 1},
-            {'id': 2, 'parent': 0, 'name': 2},
-            {'id': 3, 'parent': 1, 'name': 3},
-            {'id': 4, 'parent': 1, 'name': 4},
-            {'id': 5, 'parent': 4, 'name': 5},
-            {'id': 6, 'parent': 4, 'name': 6},
-        ]
-
-        self.assertEqual(expected, node_data, "change_parent does not work properly")
-
     def test_marker(self):
         page = pages.Tree(self._browser.driver, "TreeMarker")
         page.goto()
