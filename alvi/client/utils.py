24c276a5dbfb507b3cdb3f56ad132dc39407315c
Piotr Lewalski <piotr@lewalski.pl>
2014-01-03 10:33:19 +0100

    working on heroku support

--- alvi/client/utils.py
+++ alvi/client/utils.py
@@ -10,6 +10,7 @@ def post_to_server(path, data):
     http_client = tornado.httpclient.HTTPClient()
     json_data = dict(data=json.dumps(data))
     body = urllib.parse.urlencode(json_data)
-    url = "http://%s:%d/%s" % (options.address, options.port, path)
+    address = options.address if options.address else 'localhost'
+    url = "http://%s:%d/%s" % (address, options.port, path)
     response = http_client.fetch(url, method='POST', body=body, request_timeout=0)
     return json.loads(response.body.decode("utf-8"))
