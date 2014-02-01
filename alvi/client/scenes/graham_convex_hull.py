2a8ecaea3a3d89fa2c45c43716b57d46a63bf789
Jan Duzinkiewicz <jduzinkiewicz@gmail.com>
2014-02-01 19:02:14 +0100

    cleanup in graham convex_hull.py and cartesian.py

--- alvi/client/scenes/graham_convex_hull.py
+++ alvi/client/scenes/graham_convex_hull.py
@@ -12,42 +12,65 @@ import logging
 
 log = logging.getLogger(__package__)
 Coordinates = collections.namedtuple('Coordinates', 'x y')
+Vector = Coordinates
 PointWithDetails = collections.namedtuple('PointWithDetails', 'point angle distance')
+
+
+def angle(p0, p1):
+    p0_to_p1 = Vector(p1.x - p0.x, p1.y - p0.y)
+    len_b = math.sqrt(p0_to_p1.x**2 + p0_to_p1.y**2)
+    #derived from dot product definition - the second vector is (1, 0)
+    cos_ = p0_to_p1.x / len_b
+    return -cos_
+
+
+def distance(p0, p1):
+    section = Vector(p1.x - p0.x, p1.y - p0.y)
+    return math.sqrt(section.x**2 + section.y**2)
+
+
+def cross_product(p0, p1, p2):
+    return ((p1.x-p0.x)*(p2.y-p0.y)) - ((p2.x-p0.x)*(p1.y - p0.y))
 
 
 class LineStack():
 
     def __init__(self, container):
         self.container = container
-        self._points = []
+        self.points = []
         self._popped_since_last_sync = 0
         self._lines = []
 
     def pop(self):
-        self._points.pop()
+        self.points.pop()
         self._popped_since_last_sync += 1
 
     def push(self, point):
-        self._points.append(point)
-        if len(self._points) > 1:
+        self.points.append(point)
+        if len(self.points) > 1:
             self._sync_after_push()
 
     def _sync_after_push(self):
         if self._popped_since_last_sync == 0:
-            self._lines.append(Line(self.container, self._points[-2], self._points[-1]))
+            self._lines.append(Line(self.container, self.points[-2], self.points[-1]))
         elif self._popped_since_last_sync == 1:
-            self._lines[-1].point_to = self._points[-1]
+            self._lines[-1].point_to = self.points[-1]
             self._popped_since_last_sync = 0
         else:
             for i in range(self._popped_since_last_sync-1):
                 cartesian.remove_line(self.container._pipe,  self._lines.pop().id)
                 self.container.sync()
-            self._lines[-1].point_to = self._points[-1]
+            self._lines[-1].point_to = self.points[-1]
             self._popped_since_last_sync = 0
         self.container.sync()
 
 
 class GrahamConvexHull(base.Scene):
+    def run(self, **kwargs):
+        container = kwargs['container']
+        data_generator = kwargs['data_generator']
+        self.generate_nodes(container, data_generator)
+        self.find_convex_hull(container)
 
     def generate_nodes(self, container, data_generator):
         #create a set of unique 2d points
@@ -58,68 +81,52 @@ class GrahamConvexHull(base.Scene):
         container.sync()
 
     def find_convex_hull(self, container):
-        points = container._points
-        #find lowest leftmost point
+        lowest_leftmost_point, rest_points = self.find_lowest_leftmost(container.points)
+        rest_points = self.sort_by_angle_from_lowest(lowest_leftmost_point, rest_points)
+        rest_points = self.remove_points_with_same_angle(rest_points)
+
+        line_stack = LineStack(container)
+        line_stack.push(lowest_leftmost_point)
+        line_stack.push(rest_points[0].point)
+        line_stack.push(rest_points[1].point)
+
+        for point in rest_points[2:]:
+            while cross_product(line_stack.points[-2], line_stack.points[-1], point.point) < 0:
+                line_stack.pop()
+            line_stack.push(point.point)
+        line_stack.push(lowest_leftmost_point)
+
+    def find_lowest_leftmost(self, points):
         sorted_points = sorted(points, key=operator.attrgetter('y', 'x'))
-        #sort all other points by angle and distance from origin, if same angle use only point that is further away
-        #since p0 is lowest leftmost, we know that if it is center of polar coors system, angles will be in 0, pi range
-        #so, we can use -cos as a sorting key function
-        p0 = sorted_points[0]
-        def calculate_angle_from_p0(p):
-            a = (1, 0)
-            b = (p.x - p0.x, p.y - p0.y)
-            len_b = math.sqrt((b[0]**2 + b[1]**2))
-            dot_product = (a[0]*b[0] + a[1]*b[1])
-            cos_ = dot_product / len_b #len_a == 1
-            return -cos_
+        lowest_leftmost_point = sorted_points[0]
+        return lowest_leftmost_point, sorted_points[1:]
 
-        def calculate_distance_from_p0(p):
-            b = (p.x - p0.x, p.y - p0.y)
-            return math.sqrt((b[0]**2 + b[1]**2))
-
+    def sort_by_angle_from_lowest(self, origin_point,  points):
+        p0 = origin_point
         points_with_angles_and_distances = [
-            PointWithDetails(p, calculate_angle_from_p0(p), calculate_distance_from_p0(p)) for p in sorted_points[1:]
+            PointWithDetails(p, angle(p0, p), distance(p0, p)) for p in points
         ]
+        all_other_points = sorted(
+            points_with_angles_and_distances,
+            key=operator.attrgetter('angle', 'distance'))
+        return all_other_points
 
-        all_other_points = sorted(points_with_angles_and_distances, key=operator.attrgetter('angle', 'distance'))
-
+    def remove_points_with_same_angle(self, rest_points):
+        """
+        Point sorted by angle and distance from origin
+        - if angle is the same, leaves only the furthest one
+        """
         all_other_points_unique = []
-        last_point = all_other_points[-1]
+        last_point = rest_points[-1]
         all_other_points_unique.append(last_point)
-        for current_point in reversed(all_other_points[:-1]):
+        for current_point in reversed(rest_points[:-1]):
             old_angle = last_point.angle
             new_angle = current_point.angle
             if old_angle != new_angle:
                 all_other_points_unique.append(current_point)
                 last_point = current_point
         all_other_points_unique = list(reversed(all_other_points_unique))
-
-        line_stack = LineStack(container)
-        line_stack.push(p0)
-        line_stack.push(all_other_points_unique[0].point)
-        container.sync()
-        line_stack.push(all_other_points_unique[1].point)
-        container.sync()
-
-        def cross_product(p0, p1, p2):
-            return ((p1.x-p0.x)*(p2.y-p0.y)) - ((p2.x-p0.x)*(p1.y - p0.y))
-        for pX in all_other_points_unique[2:]:
-            while True:
-                cross = cross_product(line_stack._points[-2], line_stack._points[-1], pX.point)
-                if cross < 0:
-                    line_stack.pop()
-                else:
-                    break
-            line_stack.push(pX.point)
-            container.sync()
-        line_stack.push(p0)
-        container.sync()
-
-    def run(self, **kwargs):
-        cartesian = kwargs['container']
-        data_generator = kwargs['data_generator']
-        self.generate_nodes(cartesian, data_generator)
-        self.find_convex_hull(cartesian)
+        return all_other_points_unique
 
     @staticmethod
     def container_class():
--- alvi/containers/list/cartesian.py
+++ alvi/containers/list/cartesian.py
@@ -13,15 +13,19 @@ class Cartesian(base.List):
     def update_node(self, id, value):
         return dict(id=id, y=value)
 
-    @action
-    def create_marker(self, id, name, node_id):
-        return dict(id=id, name=name, point_id=node_id)
-
     @action('create_point')
     def create_node(self, id, parent_id, value):
         self._next_x += 1
         return dict(id=id, x=self._next_x, y=value)
 
     @action
+    def create_marker(self, id, name, node_id):
+        return dict(id=id, name=name, point_id=node_id)
+
+    @action
     def move_marker(self, id, node_id):
         return dict(id=id, point_id=node_id)
+
+    @action
+    def remove_marker(self, **kwargs):
+        return kwargs
