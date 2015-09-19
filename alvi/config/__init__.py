0d37ee8f49a121150307fb81014a15e24303ba74
Piotr Lewalski <piotr@lewalski.pl>
2015-09-19 11:11:50 +0200

    adding docker support

--- alvi/config/__init__.py
+++ alvi/config/__init__.py
@@ -42,7 +42,7 @@ def configure(config_path=None, already_loaded=[]):
 
         tornado.options.define("config", help="config file", type=str)
         tornado.options.define("port", help="server port", default=8000, type=int)
-        tornado.options.define("address", help="server address", default="127.0.0.1", type=str)
+        tornado.options.define("address", help="server address", default="0.0.0.0", type=str)
         tornado.options.define("default_scenes", help="list of auto-loaded scenes", multiple=True)
 
         #configure logging
